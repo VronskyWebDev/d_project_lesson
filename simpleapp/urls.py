94fc440aa86a4d1cfa20cfcea79497c22b6f3b20
Alexey <alexey.vronsky.studio@gmail.com>
2021-06-06 18:17:45 +0300

    Begin study filters section. Wee've created default|value

--- simpleapp/urls.py
+++ simpleapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProductsList
+from .views import ProductsList, ProductDetail
 
 urlpatterns = [
-   path("", ProductsList.as_view())
+   path("", ProductsList.as_view()),
+   path("<int:pk>", ProductDetail.as_view())
 ]
--- simpleapp/views.py
+++ simpleapp/views.py
@@ -1,7 +1,9 @@
+from datetime import datetime
 
 from django.views.generic import ListView  # импортируем класс, который
 # говорит нам о том, что в этом представлении
 # мы будем выводить список объектов из БД
+from django.views.generic import DetailView
 from .models import Product
 
 
@@ -9,6 +11,18 @@ class ProductsList(ListView):
     model = Product  # указываем модель, объекты которой мы будем выводить
     template_name = 'products.html'
     context_object_name = 'products'
+    queryset = Product.objects.order_by('-id')
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['value1'] = None
+        return context
+
+
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'product.html'
+    context_object_name = 'product'
 
 
